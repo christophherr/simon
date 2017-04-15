5ae391540e995f736c9b1a4bf32005d2b8a33762
christophherr <info@christophherr.com>
2017-04-15 11:11:26 -0400

    Moves functions into the Simon object

--- assets/app.js
+++ assets/app.js
@@ -1,74 +1,164 @@
 /* global $ */
 var Simon = {
-        count: 0,
-        activeSimon: [],
-        player: [],
-        strict: false,
-        start: false,
-        possibilities: ['#green', '#red', '#blue', '#yellow'],
-        sound: {
-            blue: new Audio(
-                'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'
-            ),
-            red: new Audio(
-                'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'
-            ),
-            yellow: new Audio(
-                'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'
-            ),
-            green: new Audio(
-                'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
-            )
+    count: 0,
+    activeSimon: [],
+    player: [],
+    strict: false,
+    start: false,
+    possibilities: ['#green', '#red', '#blue', '#yellow'],
+    sound: {
+        blue: new Audio(
+            'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'
+        ),
+        red: new Audio(
+            'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'
+        ),
+        yellow: new Audio(
+            'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'
+        ),
+        green: new Audio(
+            'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
+        )
+    },
+    resetPlayer: function() {
+        this.player = [];
+    },
+    stopGame: function() {
+        this.activeSimon = [];
+        this.count = 0;
+        setTimeout(
+            function() {
+                $('.steps-number').html(Simon.count);
+            },
+            200
+        );
+    },
+    resetGame: function() {
+        this.stopGame();
+        this.addCount();
+    },
+    startNewGame: function() {
+        this.resetGame();
+    },
+    nextLevel: function() {
+        this.addCount();
+    },
+    generateMove: function() {
+        this.activeSimon.push(this.possibilities[Math.floor(Math.random() * 4)]);
+        this.showCount();
+    },
+    addCount: function() {
+        this.count++;
+        setTimeout(
+            function() {
+                $('.steps-number').html(Simon.count);
+            },
+            200
+        );
+        this.generateMove();
+    },
+    showCount: function() {
+        var i = 0;
+        var count = setInterval(
+            function() {
+                Simon.playGame(Simon.activeSimon[i]);
+                i++;
+                if (i >= Simon.activeSimon.length) {
+                    clearInterval(count);
+                }
+            },
+            800
+        );
+        this.resetPlayer();
+    },
+    playSound: function(color) {
+        switch (color) {
+            case '#green':
+                this.sound.green.play();
+                break;
+            case '#red':
+                this.sound.red.play();
+                break;
+            case '#blue':
+                this.sound.blue.play();
+                break;
+            case '#yellow':
+                this.sound.yellow.play();
+                break;
+            default:
+                console.log(
+                    'If default is reached, something is really wrong...'
+                );
         }
     },
-    resetPlayer,
-    stopGame,
-    resetGame,
-    startNewGame,
-    nextLevel,
-    addCount,
-    generateMove,
-    showCount,
-    sound,
-    addMove,
-    playGame,
-    playerMove,
-    playerMoveIncomplete,
-    check;
+    addMove: function(id) {
+        var panel = '#' + id;
+        console.log(panel);
+        this.player.push(panel);
+        this.playerMove(panel);
+        this.playGame(panel);
+    },
+    playGame: function(panel) {
+        $(panel).fadeTo(300, 1, function() {
+            Simon.playSound(panel);
+            setTimeout(
+                function() {
+                    $(panel).fadeTo(250, 0.6);
+                },
+                300
+            );
+        });
+    },
+    playerMove: function(x) {
+        var playerMoveIncomplete = this.player[this.player.length - 1] !== this.activeSimon[this.player.length - 1];
+        var check = this.player.length === this.activeSimon.length;
+        if (this.start === false) {
+            return;
+        }
+        if (playerMoveIncomplete) {
+            if (this.strict) {
+                alert('Sorry, you made a mistake. Please start over.');
+                this.startNewGame();
+            } else {
+                alert(
+                    'Wrong move! Please look and listen carefully and correct your mistake.'
+                );
+                this.showCount();
+            }
+        } else {
+            console.log('Good Move!');
+            this.playSound(x);
 
-resetPlayer = function() {
-    Simon.player = [];
+            if (check) {
+                if (this.count === 20) {
+                    alert(
+                        'You won! Congratulations. Do you want to play another round?'
+                    );
+                } else {
+                    this.nextLevel();
+                }
+            }
+        }
+    }
 };
 
-stopGame = function() {
-    Simon.activeSimon = [];
-    Simon.count = 0;
-    setTimeout(
-        function() {
-            $('.steps-number').html(Simon.count);
-        },
-        200
-    );
-};
 
-resetGame = function() {
-    stopGame();
-    addCount();
-};
+$('.panel.green, .panel.red, .panel.blue, .panel.yellow').click(function() {
+    $(this).fadeTo(350, 1, function() {
+        $(this).fadeTo(250, 0.6);
+    });
+});
 
-startNewGame = function() {
-    resetGame();
-};
 
 $('.button-restart').click(function() {
     if (Simon.start === false) {
         Simon.start = true;
         $(this).toggleClass('faded').css('background', 'green');
-        startNewGame();
+        Simon.startNewGame();
     } else {
         Simon.start = false;
         $(this).toggleClass('faded').css('background', 'red');
-        stopGame();
+        Simon.stopGame();
     }
 });
 
@@ -76,11 +166,11 @@ $('.button-strict').click(function() {
     if (Simon.strict === false) {
         Simon.strict = true;
         $(this).toggleClass('faded');
-        startNewGame();
+        Simon.startNewGame();
     } else {
         Simon.strict = false;
         $(this).toggleClass('faded');
-        startNewGame();
+        Simon.startNewGame();
     }
 });
 
@@ -92,123 +182,5 @@ $('.button-reset').click(function() {
     if (Simon.start === false) {
         return;
     }
-    startNewGame();
+    Simon.startNewGame();
 });
-
-nextLevel = function() {
-    addCount();
-};
-
-addCount = function() {
-    Simon.count++;
-    setTimeout(
-        function() {
-            $('.steps-number').html(Simon.count);
-        },
-        200
-    );
-
-    generateMove();
-};
-
-generateMove = function() {
-    Simon.activeSimon.push(Simon.possibilities[Math.floor(Math.random() * 4)]);
-
-    showCount();
-};
-
-showCount = function() {
-    var i = 0;
-    var count = setInterval(
-        function() {
-            playGame(Simon.activeSimon[i]);
-            i++;
-            if (i >= Simon.activeSimon.length) {
-                clearInterval(count);
-            }
-        },
-        800
-    );
-
-    resetPlayer();
-};
-
-sound = function(color) {
-    switch (color) {
-        case '#green':
-            Simon.sound.green.play();
-            break;
-        case '#red':
-            Simon.sound.red.play();
-            break;
-        case '#blue':
-            Simon.sound.blue.play();
-            break;
-        case '#yellow':
-            Simon.sound.yellow.play();
-            break;
-        default:
-            console.log(
-                'If default is reached, something is really wrong...'
-            );
-    }
-};
-
-addMove = function(id) {
-    var panel = '#' + id;
-    console.log(panel);
-    Simon.player.push(panel);
-    playerMove(panel);
-};
-
-playGame = function(panel) {
-    $(panel).fadeTo(300, 1, function() {
-        sound(panel);
-        setTimeout(
-            function() {
-                $(panel).fadeTo(250, 0.6);
-            },
-            300
-        );
-    });
-};
-
-$('.panel.green, .panel.red, .panel.blue, .panel.yellow').click(function() {
-    $(this).fadeTo(350, 1, function() {
-        $(this).fadeTo(250, 0.6);
-    });
-});
-
-playerMove = function(x) {
-    if (Simon.start === false) {
-        return;
-    }
-
-    playerMoveIncomplete = Simon.player[Simon.player.length - 1] !==
-        Simon.activeSimon[Simon.player.length - 1];
-
-    if (playerMoveIncomplete) {
-        if (Simon.strict) {
-            alert('Sorry, you made a mistake. Please start over.');
-            startNewGame();
-        } else {
-            alert(
-                'Wrong move! Please look and listen carefully and correct your mistake.'
-            );
-            showCount();
-        }
-    } else {
-        console.log('Good Move!');
-        sound(x);
-        check = Simon.player.length === Simon.activeSimon.length;
-        if (check) {
-            if (Simon.count === 20) {
-                alert(
-                    'You won! Congratulations. Do you want to play another round?'
-                );
-            } else {
-                nextLevel();
-            }
-        }
-    }
-};
